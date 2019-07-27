7d190e805b4580738c01eeb118b103454b831a7e
Pranav Singh <pranav.singh1511@gmail.com>
2019-07-27 17:31:45 +0530

    Initial commit

---
+++ src/main/java/com/pranav/interviewpipeline/InterviewPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.interviewpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pranav/interviewpipeline/InterviewPipelineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pranav.interviewpipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewPipelineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
